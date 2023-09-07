0c3c0a12d3fd3669e481be13c3060c3636d7005c
Ashish <ashish30vmc@gmail.com>
2023-09-07 14:01:45 +0200

    Fancy changes for better outlook

--- Task4.py
+++ Task4.py
@@ -1,3 +1,5 @@
+""" File to execute task 4 of LAB1 """
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -9,20 +11,16 @@ class Task4:
     def __init__(self):
         self.extract_df = task3.import_csv_to_df()
 
+    """ Function to get districts in csv """
     def get_number_of_districts(self):
-        print("Number of districts : ", self.extract_df.shape[0])
+        print(f'Number of districts :', self.extract_df.shape[0])
 
+    """ Function to get house value mean(using customized mean of task1) """
     def get_mean_house_value(self):
-        # columns = extract_df[['median_house_value']].mean()
-        # print(extract_df['median_house_value'].dtype)
-        # df_list = extract_df['median_house_value'].tolist()
         df_list = self.extract_df['median_house_value'].astype(float)
-        # print(df_list)
-        # df_list = [float(x) for x in df_list]
-        # extract_df[['median_house_value']] = pd.to_numeric(extract_df[['median_house_value']])
-        # print(extract_df['median_house_value'])
-        print(task1.customized_mean(df_list))
+        print(f'house value mean :', task1.customized_mean(df_list))
 
+    """ Function to plot histogram """
     def plot_histogram(self, column_list):
         for column in column_list:
             self.extract_df[column].astype(float).hist(bins=50)
@@ -31,6 +29,7 @@ class Task4:
             plt.title(column + ' Chart')
             plt.show()
 
+    """ Function to evaluate ocean proximity mean """
     def ocean_proximity_mean(self):
         column_name_list = []
         opm_value_list = []
@@ -48,20 +47,26 @@ class Task4:
         print(df_opm)
 
 
-task4_obj = Task4()
-
-""" 4.1: Count the number of districts loaded in this exercise """
-task4_obj.get_number_of_districts()
-
-""" 4.2: Calculate the mean of house values among all the districts """
-task4_obj.get_mean_house_value()
-
-""" 4.3: Create a histogram for amount_of_households, median_income , housing_median_age and
-median_house_value. """
-task4_obj.plot_histogram(['households', 'median_income', 'housing_median_age', 'median_house_value'])
-
-""" 4.Additional Task:  For each ocean proximity category in the dataset calculate the mean house value."""
-task4_obj.ocean_proximity_mean()
+""" Main Function to be executed """
 
 
+def main():
+    task4_obj = Task4()
+
+    """ 4.1: Count the number of districts loaded in this exercise """
+    task4_obj.get_number_of_districts()
+
+    """ 4.2: Calculate the mean of house values among all the districts """
+    task4_obj.get_mean_house_value()
+
+    """ 4.3: Create a histogram for amount_of_households, median_income , housing_median_age and
+    median_house_value. """
+    task4_obj.plot_histogram(['households', 'median_income', 'housing_median_age', 'median_house_value'])
+
+    """ 4.Additional Task:  For each ocean proximity category in the dataset calculate the mean house value."""
+    task4_obj.ocean_proximity_mean()
+
+
+if __name__ == "__main__":
+    main()
 
