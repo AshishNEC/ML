aed0b0b83e20989b6646fd7158a901f29880bf13
Ashish <ashish30vmc@gmail.com>
2023-09-08 08:36:43 +0200

    Code optimization

--- Task3.py
+++ Task3.py
@@ -1,17 +1,24 @@
+""" Task 3: Load and inspect the housing data """
+
 import pandas as pd
 
+""" Function to read and load csv file to dataframe"""
 
-def import_csv_to_df():
+
+def import_csv_to_df(csv_file):
     data = []
     flag = 1
-    with open('./housing.csv', "r") as file:
+    with open(csv_file, "r") as file:
         reader_variable = file.readlines()
         for data_value in reader_variable:
             if flag:
                 column_name = (data_value.strip().split(','))
+                flag = 0
             else:
                 data.append(data_value.strip().split(','))
 
-            df = pd.DataFrame(data, columns=column_name)
-            flag = 0
+        df = pd.DataFrame(data, columns=column_name)
     return df
+
+
+import_csv_to_df('housing.csv')
